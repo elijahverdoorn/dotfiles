a2fafc4d5cfa085f4033aacb214ba6d61c7bd5ab
Elijah Verdoorn <everdoorn@pandora.com>
2018-04-04 17:43:34 -0700

    Add checking in pandora-app-build for existence of code directory
    Also add some comments

--- fish-functions/pandora-app-build.fish
+++ fish-functions/pandora-app-build.fish
@@ -1,7 +1,11 @@
 function pandora-app-build -d 'Build and start the pandora app without Android Studio'
-	set current_dir $PWD
-	cd ~/android
-	./gradlew app:installInternalDebug
-	adb shell am start -n "com.pandora.android.internal/com.pandora.android.Main" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-	cd $current_dir
+	if test -d ~/android # check if the code directory exists
+		set current_dir $PWD # make sure we can navigate back to the user's current dir
+		cd ~/android # this is where I put the android repo
+		./gradlew app:installInternalDebug
+		adb shell am start -n "com.pandora.android.internal/com.pandora.android.Main" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+		cd $current_dir # put the user back where they started
+	else
+		echo Code directory not found, aborting
+	end
 end
