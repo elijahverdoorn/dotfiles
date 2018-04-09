e8c45ae6efa04b8dd02ce6cb11c80d12fe96f68e
Elijah Verdoorn <everdoorn@pandora.com>
2018-04-09 10:13:50 -0700

    Change git branch in prompt to be smaller

--- fish-functions/fish_prompt.fish
+++ fish-functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt # Custom prompt for fish shell
 	if git rev-parse --git-dir > /dev/null ^ /dev/null
-		echo (pwd) '>' (git rev-parse --abbrev-ref HEAD) '> '
+		echo (pwd) '('(git rev-parse --abbrev-ref HEAD)') > '
 	else
 		echo (pwd) '> '
 	end
