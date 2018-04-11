6440c655ee84d1990c60a0f39b2996e4d3d4fd18
Elijah Verdoorn <everdoorn@pandora.com>
2018-04-11 11:06:01 -0700

    Add colors to fish prompt

--- fish-functions/fish_prompt.fish
+++ fish-functions/fish_prompt.fish
@@ -1,7 +1,13 @@
 function fish_prompt # Custom prompt for fish shell
 	if git rev-parse --git-dir > /dev/null ^ /dev/null
-		echo (pwd) '('(git rev-parse --abbrev-ref HEAD)') > '
+		set_color blue
+		echo -n (pwd)
+		set_color green
+		echo -n ' ('(git rev-parse --abbrev-ref HEAD)')'
+		set_color blue
+		echo -n ' > '
 	else
+		set_color blue
 		echo (pwd) '> '
 	end
 end
