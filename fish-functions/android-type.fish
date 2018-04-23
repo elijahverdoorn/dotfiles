d0edd1cf81057f4644811b582557e2ff09ba8590
Elijah Verdoorn <everdoorn@pandora.com>
2018-04-23 13:49:48 -0700

    Prevent android-type function from adding a space after last word

--- fish-functions/android-type.fish
+++ fish-functions/android-type.fish
@@ -1,8 +1,12 @@
 function android-type -d "send text to connected android device"
+	set used_argv_count 0
 	if count $argv > /dev/null
 		for x in $argv
 			adb shell input text $x
-			adb shell input keyevent 62
+			set used_argv_count (math $used_argv_count + 1)
+			if test (count $argv) -gt $used_argv_count
+				adb shell input keyevent 62
+			end
 		end
 	end
 end
