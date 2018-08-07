153985c4cb1778a7d4d23b8c660a24e377031189
Elijah Verdoorn <everdoorn@pandora.com>
2018-08-07 14:53:51 -0700

    Move development directory and update scripts to match

--- fish-functions/pandora-app-build.fish
+++ fish-functions/pandora-app-build.fish
@@ -1,7 +1,8 @@
 function pandora-app-build -d 'Build and start the pandora app without Android Studio'
-	if test -d ~/android # check if the code directory exists
+	set dev_dir ~/dev/android # this is where I put the android repo
+	if test -d $dev_dir # check if the code directory exists
 		set current_dir $PWD # make sure we can navigate back to the user's current dir
-		cd ~/android # this is where I put the android repo
+		cd $dev_dir
 		./gradlew app:installInternalDebug
 		adb shell am start -n "com.pandora.android.internal/com.pandora.android.Main" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
 		cd $current_dir # put the user back where they started
