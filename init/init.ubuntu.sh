da598ca84cbfc1bd8a859c7ceb6bf6fdcbeb5241
Elijah Verdoorn <elijah@elijahverdoorn.com>
2020-05-02 10:44:47 -0700

    Add Ubuntu init script, move osx init to new file name

---
+++ init/init.ubuntu.sh
@@ -0,0 +1,7 @@
+apt install git
+apt install vim
+
+git config --global user.name "Elijah Verdoorn"
+git config --global user.email "elijah@elijahverdoorn.com"
+git config --global core.editor $(which vim)
+
