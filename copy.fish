e9880c0266eb4efc7478c1e7bc4e1814b74bc6f1
Elijah Verdoorn <elijah.verdoorn@robinhood.com>
2021-05-12 10:14:46 -0500

    Update copy.fish to setup config.fish automatically

--- copy.fish
+++ copy.fish
@@ -4,3 +4,7 @@ set --local config ~/.config/fish/
 rm -rf ~/.config/fish/functions/
 mkdir ~/.config/fish/functions/
 cp -v ./fish-functions/* ~/.config/fish/functions/
+
+cp config.fish ~/.config/fish/config.fish
+
+exec fish
