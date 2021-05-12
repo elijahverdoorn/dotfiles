d2d644530082caf0676e30303e559a0b49fd257c
Elijah Verdoorn <elijah.verdoorn@robinhood.com>
2021-05-12 10:13:25 -0500

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,6 @@
+pyenv init - | source
+
+set -x WORKON_HOME "(HOME)/.virtualenvs"
+set -x PROJECT_HOME "/Users/elijah.verdoorn/robinhood"
+
+alias bonfire "RUN_LOCALLY=1 python scripts/run/bonfire_cli.py"
